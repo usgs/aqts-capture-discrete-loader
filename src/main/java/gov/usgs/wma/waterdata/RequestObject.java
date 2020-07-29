db000fa34642a6c722929f2d29991d7ab6fa3dc8
ssoper <ssoper@usgs.gov>
2020-07-29 10:11:41 -0500

    remove unused code

--- src/main/java/gov/usgs/wma/waterdata/RequestObject.java
+++ src/main/java/gov/usgs/wma/waterdata/RequestObject.java
@@ -11,12 +11,4 @@ public class RequestObject {
 	public void setLocationIdentifier(String locationIdentifier) {
 		this.locationIdentifier = locationIdentifier;
 	}
-	//	private List<String> fieldVisitIdentifiers;
-//
-//	public List<String> getFieldVisitIdentifiers() {
-//		return fieldVisitIdentifiers;
-//	}
-//	public void setFieldVisitIdentifiers(List<String> fieldVisitIdentifiers) {
-//		this.fieldVisitIdentifiers = fieldVisitIdentifiers;
-//	}
 }
