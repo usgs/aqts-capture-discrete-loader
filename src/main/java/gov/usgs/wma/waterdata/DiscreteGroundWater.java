e21e1878fb17b6f05be2d677dd9e8cd5cc0a8b44
Eric Everman <eeverman@usgs.gov>
2020-10-02 13:24:35 -0500

    Fix a small equals and hashcode omision

--- src/main/java/gov/usgs/wma/waterdata/DiscreteGroundWater.java
+++ src/main/java/gov/usgs/wma/waterdata/DiscreteGroundWater.java
@@ -314,6 +314,7 @@ public class DiscreteGroundWater {
 		return java.util.Objects.equals(fieldVisitIdentifier, that.fieldVisitIdentifier) &&
 				java.util.Objects.equals(locationIdentifier, that.locationIdentifier) &&
 				java.util.Objects.equals(agencyCode, that.agencyCode) &&
+				java.util.Objects.equals(dateTimeAccuracyCode, that.dateTimeAccuracyCode) &&
 				java.util.Objects.equals(startTime, that.startTime) &&
 				java.util.Objects.equals(endTime, that.endTime) &&
 				java.util.Objects.equals(party, that.party) &&
@@ -349,6 +350,7 @@ public class DiscreteGroundWater {
 				fieldVisitIdentifier,
 				locationIdentifier,
 				agencyCode,
+				dateTimeAccuracyCode,
 				startTime,
 				endTime,
 				party,
